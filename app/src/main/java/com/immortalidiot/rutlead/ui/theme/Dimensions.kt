0ffdb64e7e700309b2027f7f45c46d415c65dd79
ImmortalIdiot <max.krasulya@yandex.ru>
2024-03-29 03:47:48 +0300

    feat: add the necessary dimensions; refactor: change the dimension value

--- app/src/main/java/com/immortalidiot/rutlead/ui/theme/Dimensions.kt
+++ app/src/main/java/com/immortalidiot/rutlead/ui/theme/Dimensions.kt
@@ -13,9 +13,12 @@ data class Dimensions(
     val verticalXLarge: Dp = 32.dp,
     val verticalXXLarge: Dp = 40.dp,
 
+    val verticalXSSmallPadding: Dp = 1.dp,
     val verticalSmallPadding: Dp = 4.dp,
-    val verticalNormalPadding: Dp = 6.dp,
+    val verticalNormalPadding: Dp = 8.dp,
     val verticalBigPadding: Dp = 16.dp,
+
+    val bigPadding: Dp = 16.dp,
 
     val shapeSSmall: Dp = 2.dp,
     val shapeXSmall: Dp = 4.dp,
