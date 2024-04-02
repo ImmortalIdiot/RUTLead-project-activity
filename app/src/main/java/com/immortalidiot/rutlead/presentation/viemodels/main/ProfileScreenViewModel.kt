a9fe185ef9a28333cf7c8a3e077f324a895218bc
ImmortalIdiot <max.krasulya@yandex.ru>
2024-04-03 01:41:53 +0300

    refactor: add theme manager in the viewmodel

--- app/src/main/java/com/immortalidiot/rutlead/presentation/viemodels/main/ProfileScreenViewModel.kt
+++ app/src/main/java/com/immortalidiot/rutlead/presentation/viemodels/main/ProfileScreenViewModel.kt
@@ -3,34 +3,42 @@ package com.immortalidiot.rutlead.presentation.viemodels.main
 import androidx.compose.runtime.Immutable
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.update
 
 class ProfileScreenViewModel : ViewModel() {
+
+    private val themeFlow = ThemeManager.themeFlow
     @Immutable
     sealed class State {
-        object AutoThemeSelected: State()
-        object LightThemeSelected: State()
-        object DarkThemeSelected: State()
+        object AutoThemeSelected : State()
+        object LightThemeSelected : State()
+        object DarkThemeSelected : State()
     }
 
     var mutableState = MutableStateFlow<State>(State.AutoThemeSelected)
         private set
 
     fun onAutoTheme() {
-        mutableState.update {
-            State.AutoThemeSelected
-        }
+        mutableState.value = State.AutoThemeSelected
+        themeFlow.value = ThemeStyle.AUTO
     }
 
     fun onDarkTheme() {
-        mutableState.update {
-            State.DarkThemeSelected
-        }
+        mutableState.value = State.DarkThemeSelected
+        themeFlow.value = ThemeStyle.DARK
     }
 
     fun onLightTheme() {
-        mutableState.update {
-            State.LightThemeSelected
-        }
+        mutableState.value = State.LightThemeSelected
+        themeFlow.value = ThemeStyle.LIGHT
     }
+}
+
+object ThemeManager {
+    val themeFlow: MutableStateFlow<ThemeStyle> = MutableStateFlow(ThemeStyle.AUTO)
+}
+
+enum class ThemeStyle {
+    DARK,
+    LIGHT,
+    AUTO
 }
