2945fe6034f576603aa15812b377a72bb5d92fa7
ImmortalIdiot <max.krasulya@yandex.ru>
2024-04-08 19:49:21 +0300

    refactor: change the logic of the viewmodel

--- app/src/main/java/com/immortalidiot/rutlead/presentation/viemodels/main/ProfileScreenViewModel.kt
+++ app/src/main/java/com/immortalidiot/rutlead/presentation/viemodels/main/ProfileScreenViewModel.kt
@@ -1,19 +1,24 @@
 package com.immortalidiot.rutlead.presentation.viemodels.main
 
+import androidx.compose.runtime.Immutable
 import androidx.lifecycle.ViewModel
-import com.immortalidiot.rutlead.ui.models.LogoutModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.update
 
 class ProfileScreenViewModel : ViewModel() {
-    private val _logoutUiState = MutableStateFlow(LogoutModel(isDialogVisible = false))
-    var logoutUiState: StateFlow<LogoutModel> = _logoutUiState.asStateFlow()
+    @Immutable
+    sealed class State {
+        object Init : State()
+        object LogoutDialog : State()
+    }
 
-    fun changeLogoutDialogVisibility(isLogoutDialogVisible: Boolean) {
-        _logoutUiState.update {
-            logoutUiState.value.copy(isDialogVisible = !isLogoutDialogVisible)
-        }
+    var mutableState = MutableStateFlow<State>(State.Init)
+        private set
+
+    fun changeLogoutDialogVisibility() {
+        mutableState.value = State.LogoutDialog
+    }
+
+    fun onCancelled() {
+        mutableState.value = State.Init
     }
 }
