2189e366818eb29e106af3ebd8e82875243fed98
ImmortalIdiot <max.krasulya@yandex.ru>
2024-04-12 02:21:11 +0300

    feat: add ui interaction

--- app/src/main/java/com/immortalidiot/rutlead/presentation/viemodels/main/ProfileScreenViewModel.kt
+++ app/src/main/java/com/immortalidiot/rutlead/presentation/viemodels/main/ProfileScreenViewModel.kt
@@ -2,7 +2,11 @@ package com.immortalidiot.rutlead.presentation.viemodels.main
 
 import androidx.compose.runtime.Immutable
 import androidx.lifecycle.ViewModel
+import com.immortalidiot.rutlead.ui.models.ChangeGroupModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class ProfileScreenViewModel : ViewModel() {
     @Immutable
@@ -15,6 +19,9 @@ class ProfileScreenViewModel : ViewModel() {
     var mutableState = MutableStateFlow<State>(State.Init)
         private set
 
+    private val _uiState = MutableStateFlow(ChangeGroupModel(group = String()))
+    val uiState: StateFlow<ChangeGroupModel> = _uiState.asStateFlow()
+
     fun changeLogoutDialogVisibility() {
         mutableState.value = State.LogoutDialog
     }
@@ -25,5 +32,11 @@ class ProfileScreenViewModel : ViewModel() {
 
     fun changeGroupDialogVisibility() {
         mutableState.value = State.ChangeGroupDialog
+    }
+
+    fun changeGroup(group: String) {
+        _uiState.update {
+            uiState.value.copy(group = group)
+        }
     }
 }
