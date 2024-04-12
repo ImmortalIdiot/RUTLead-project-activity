d5fb86ec6e453cd4caff1a901f7d61f458478d60
ImmortalIdiot <max.krasulya@yandex.ru>
2024-04-13 02:24:18 +0300

    feat: add snackbar content params

--- app/src/main/java/com/immortalidiot/rutlead/ui/components/dialogs/PrimaryProfileDialog.kt
+++ app/src/main/java/com/immortalidiot/rutlead/ui/components/dialogs/PrimaryProfileDialog.kt
@@ -3,13 +3,19 @@ package com.immortalidiot.rutlead.ui.components.dialogs
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -31,7 +37,9 @@ fun PrimaryProfileDialog(
     headerTextStyle: TextStyle,
     headerTextColor: Color,
     onCancelled: () -> Unit,
-    content: @Composable () -> Unit,
+    isSnackbar: Boolean,
+    snackbarHostState: SnackbarHostState? = null,
+    content: @Composable () -> Unit
 ) {
     val dimensions = LocalDimensions.current
     val scheme = MaterialTheme.colorScheme
@@ -42,28 +50,51 @@ fun PrimaryProfileDialog(
         onDismissRequest = { onCancelled() },
         properties = properties
     ) {
-        Column(
+        Box(
             modifier = modifier
-                .heightIn(max = 200.dp)
-                .widthIn(max = 300.dp)
-                .fillMaxWidth()
                 .fillMaxHeight()
-                .clip(roundedShape)
-                .background(color = scheme.onBackground)
-                .border(
-                    width = LocalDimensions.current.borderXSSmall,
-                    color = ClassicGray,
-                    shape = roundedShape
-                ),
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
+                .verticalScroll(rememberScrollState()),
+            contentAlignment = Alignment.Center
         ) {
-            Text(
-                text = headerText,
-                style = headerTextStyle,
-                color = headerTextColor
-            )
-            content()
+            Column(
+                modifier = modifier
+                    .heightIn(max = 200.dp)
+                    .widthIn(max = 300.dp)
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .clip(roundedShape)
+                    .background(color = scheme.onBackground)
+                    .border(
+                        width = LocalDimensions.current.borderXSSmall,
+                        color = ClassicGray,
+                        shape = roundedShape
+                    ),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = headerText,
+                    style = headerTextStyle,
+                    color = headerTextColor
+                )
+                content()
+            }
+            if (isSnackbar) {
+                SnackbarHost(
+                    modifier = modifier
+                        .fillMaxWidth()
+                        .align(Alignment.BottomCenter),
+                    hostState = snackbarHostState!!
+                ) {
+                    Snackbar(
+                        snackbarData = it,
+                        containerColor = scheme.errorContainer,
+                        contentColor = scheme.onPrimary,
+                        dismissActionContentColor = scheme.onPrimary,
+                        shape = RoundedCornerShape(LocalDimensions.current.shapeNormal)
+                    )
+                }
+            }
         }
     }
 }
