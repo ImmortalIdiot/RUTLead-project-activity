c6e507c85a8538e1c4efbec613ad1715a23253f6
ImmortalIdiot <max.krasulya@yandex.ru>
2024-04-14 21:13:02 +0300

    feat: send the user to the main screen flow if registration is successful

--- app/src/main/java/com/immortalidiot/rutlead/presentation/screens/auth/SignUpScreen.kt
+++ app/src/main/java/com/immortalidiot/rutlead/presentation/screens/auth/SignUpScreen.kt
@@ -16,11 +16,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -37,11 +35,10 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
 import com.immortalidiot.rutlead.R
 import com.immortalidiot.rutlead.navigation.auth.AuthScreen
+import com.immortalidiot.rutlead.navigation.main.MainScreen
 import com.immortalidiot.rutlead.ui.components.buttons.PrimaryButton
 import com.immortalidiot.rutlead.ui.components.other.BottomSnackbar
 import com.immortalidiot.rutlead.ui.components.other.BoxLabel
@@ -129,6 +126,16 @@ fun SignUpScreen(
             }
         }
     }
+
+    if (state is SignUpViewModel.State.Success) {
+        navHostController.navigate(MainScreen.ProfileScreen.route) {
+            popUpTo(0) {
+                inclusive = false
+                saveState = true
+            }
+        }
+    }
+
     Column(
         modifier = modifier
             .fillMaxSize()
