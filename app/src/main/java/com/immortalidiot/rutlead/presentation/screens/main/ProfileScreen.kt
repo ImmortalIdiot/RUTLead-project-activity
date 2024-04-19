72d0543af7666a165924ca2f4091a1714d2f9cc3
ImmortalIdiot <max.krasulya@yandex.ru>
2024-04-20 01:40:25 +0300

    refactor: completely remove dialog scrolls

--- app/src/main/java/com/immortalidiot/rutlead/presentation/screens/main/ProfileScreen.kt
+++ app/src/main/java/com/immortalidiot/rutlead/presentation/screens/main/ProfileScreen.kt
@@ -14,9 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -197,9 +195,7 @@ fun ProfileScreen(
         profileState is ProfileScreenViewModel.State.GroupValidationError
     ) {
         Box(
-            modifier = modifier
-                .fillMaxHeight()
-                .verticalScroll(rememberScrollState()),
+            modifier = modifier.fillMaxHeight(),
             contentAlignment = Alignment.Center
         ) {
             PrimaryProfileDialog(
@@ -302,9 +298,7 @@ fun ProfileScreen(
 
     if (profileState is ProfileScreenViewModel.State.DeleteAccountDialog) {
         Box(
-            modifier = modifier
-                .fillMaxHeight()
-                .verticalScroll(rememberScrollState()),
+            modifier = modifier.fillMaxHeight(),
             contentAlignment = Alignment.Center
         ) {
             PrimaryProfileDialog(
