0810ed3ca2d6f7a2031a80e535d0ed129fa30418
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-06 02:57:23 +0300

    refactor: use student model in the register function

--- app/src/main/java/com/immortalidiot/rutlead/database/ServiceAPI.kt
+++ app/src/main/java/com/immortalidiot/rutlead/database/ServiceAPI.kt
@@ -6,21 +6,8 @@ import retrofit2.http.POST
 
 interface ServiceAPI {
     @POST("/student")
-    fun register(@Body studentRequest: StudentRequest): Call<StudentRequest>
+    fun register(@Body studentRequest: Student): Call<Student>
 
     @POST
     fun auth(@Body auth: Student): Call<Token>
 }
-
-data class StudentRequest(
-    val studentID: Int,
-    val password: String,
-    val email: String,
-    val fullName: String,
-    val group: String
-)
-
-data class StudentResponse(
-    val studentID: Int,
-    val fullName: String,
-)
