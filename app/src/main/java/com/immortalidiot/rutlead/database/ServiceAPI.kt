7afdb958ab5409fcc98c1870273e91890141fc67
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-07 01:40:51 +0300

    refactor: make register function suspend; change return type

--- app/src/main/java/com/immortalidiot/rutlead/database/ServiceAPI.kt
+++ app/src/main/java/com/immortalidiot/rutlead/database/ServiceAPI.kt
@@ -1,12 +1,13 @@
 package com.immortalidiot.rutlead.database
 
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ServiceAPI {
     @POST("/student")
-    fun register(@Body studentRequest: Student): Call<Student>
+    suspend fun register(@Body newStudent: Student): Response<Unit>
 
     @POST
     fun auth(@Body auth: Student): Call<Token>
