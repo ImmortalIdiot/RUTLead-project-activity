fe83d3050b7f0324c335ca686f817f08b452b017
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-15 04:15:00 +0300

    refactor: replace result return type with an call<response>

--- app/src/main/java/com/immortalidiot/rutlead/database/ServiceAPI.kt
+++ app/src/main/java/com/immortalidiot/rutlead/database/ServiceAPI.kt
@@ -1,13 +1,12 @@
 package com.immortalidiot.rutlead.database
 
 import retrofit2.Call
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface ServiceAPI {
     @POST("/student")
-    suspend fun register(@Body newStudent: Student): Response<Unit>
+    suspend fun register(@Body newStudent: Student): Call<RegistrationResponse>
 
     @POST
     fun auth(@Body auth: Student): Call<Token>
