f393e9f27368035fe3a3118003eec7f7ad9b0981
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-16 04:16:28 +0300

    feat: add a message class

---
+++ app/src/main/java/com/immortalidiot/rutlead/util/Messages.kt
@@ -0,0 +1,8 @@
+package com.immortalidiot.rutlead.util
+
+object Messages {
+    const val SUCCESSFUL_REGISTRATION = "Успешная регистрация"
+    const val SUCCESSFUL_LOGIN = "Успешный вход"
+    const val UNKNOWN_ERROR = "Неизвестная ошибка, попробуйте позднее"
+    const val NETWORK_ERROR = "Ошибка сети"
+}
