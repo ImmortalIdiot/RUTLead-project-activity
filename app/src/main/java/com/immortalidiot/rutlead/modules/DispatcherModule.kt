76453cdfbc45d6578cee763feec709f7ed247141
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-16 04:08:11 +0300

    feat: create dispatcher module for the io dispatcher

---
+++ app/src/main/java/com/immortalidiot/rutlead/modules/DispatcherModule.kt
@@ -0,0 +1,25 @@
+package com.immortalidiot.rutlead.modules
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class Dispatcher(val qualifier: String) {
+    companion object {
+        const val IO = "IO"
+    }
+}
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DispatcherModule {
+    @Provides
+    @Dispatcher(Dispatcher.IO)
+    fun providesIODispatcher(): CoroutineDispatcher = Dispatchers.IO
+}
