766afc657eff9811425b1d70bd190e73c04ef6b2
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-16 04:51:15 +0300

    feat: add advice if network error

--- app/src/main/java/com/immortalidiot/rutlead/util/Messages.kt
+++ app/src/main/java/com/immortalidiot/rutlead/util/Messages.kt
@@ -4,5 +4,5 @@ object Messages {
     const val SUCCESSFUL_REGISTRATION = "Успешная регистрация"
     const val SUCCESSFUL_LOGIN = "Успешный вход"
     const val UNKNOWN_ERROR = "Неизвестная ошибка, попробуйте позднее"
-    const val NETWORK_ERROR = "Ошибка сети"
+    const val NETWORK_ERROR = "Ошибка сети. Проверьте подключение к Интернету"
 }
