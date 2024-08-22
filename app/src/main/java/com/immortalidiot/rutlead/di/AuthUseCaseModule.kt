9cc030051e134a944f3acc21a41fec45a1c027ea
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-22 23:18:31 +0300

    chore: merge the login and registration use case modules

--- app/src/main/java/com/immortalidiot/rutlead/di/AuthUseCaseModule.kt
+++ app/src/main/java/com/immortalidiot/rutlead/di/AuthUseCaseModule.kt
@@ -2,6 +2,8 @@ package com.immortalidiot.rutlead.di
 
 import com.immortalidiot.rutlead.usecases.LoginUseCase
 import com.immortalidiot.rutlead.usecases.LoginUseCaseImpl
+import com.immortalidiot.rutlead.usecases.RegistrationUseCase
+import com.immortalidiot.rutlead.usecases.RegistrationUseCaseImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -10,8 +12,12 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface LoginUseCaseModule {
+interface AuthUseCaseModule {
     @Binds
     @Singleton
     fun provideLoginUseCase(impl: LoginUseCaseImpl): LoginUseCase
+    
+    @Binds
+    @Singleton
+    fun provideRegistrationUseCase(impl: RegistrationUseCaseImpl): RegistrationUseCase
 }
