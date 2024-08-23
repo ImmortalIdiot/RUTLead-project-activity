a4df54171e1bf5acf333f998ff3705fe00fd79d7
ImmortalIdiot <max.krasulya@yandex.ru>
2024-08-24 00:40:03 +0300

    feat: create module for the service

---
+++ app/src/main/java/com/immortalidiot/rutlead/di/NetworkModule.kt
@@ -0,0 +1,28 @@
+package com.immortalidiot.rutlead.di
+
+import com.immortalidiot.rutlead.BuildConfig
+import com.immortalidiot.rutlead.database.ServiceAPI
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Provides
+    fun provideServiceAPI(retrofit: Retrofit): ServiceAPI {
+        return retrofit.create(ServiceAPI::class.java)
+    }
+}
